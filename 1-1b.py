dabe2a78c9f6ab21ec3a2909d4bd8bb18f7180fc
hiroaki matsumoto <53863240+is0378hf@users.noreply.github.com>
2019-08-31 16:52:15 +0900

    Add files via upload

---
+++ 1-1b.py
@@ -0,0 +1,6 @@
+print("input 1~100")
+x = input()
+if 1<=x<=100:
+  print(int(x))
+else:
+  print("overflow")
---
+++ 1-1c.py
@@ -0,0 +1,5 @@
+a,b = input().split()
+c = int(a) * int(b)
+d = int(a)*2 + int(b)*2
+print(c)
+print(d)

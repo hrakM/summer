26e5843de7255bd650e0ca627f3bbae99ad34953
hiroaki matsumoto <53863240+is0378hf@users.noreply.github.com>
2019-09-05 15:17:32 +0900

    Add files via upload

---
+++ 1-3c.py
@@ -0,0 +1,9 @@
+print("0 0と入力すると終了します。")
+print('任意の数値を入力してください。: ')
+l= list(iter(input,'0 0'))
+m=0
+#l=" \n".join(l)
+nw=sorted(l)
+print('Input:\n'+str(l))
+nw=" \n".join(nw)
+print('Output:\n'+str(nw))
